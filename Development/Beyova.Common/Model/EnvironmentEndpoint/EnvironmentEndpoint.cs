37c8d026951f96b8d051a69f7ad6c51bebd7ce29
Rynn Wang <rynn.wang@outlook.com>
2015-12-19 23:13:04 +0800

    no message

--- Development/Beyova.Common/Model/EnvironmentEndpoint/EnvironmentEndpoint.cs
+++ Development/Beyova.Common/Model/EnvironmentEndpoint/EnvironmentEndpoint.cs
@@ -10,15 +10,51 @@ using Beyova.RestApi;
 namespace Beyova.Model
 {
     /// <summary>
-    /// Class EnvironmentEndpointCriteria.
+    /// Class EnvironmentEndpoint.
     /// </summary>
-    public class EnvironmentEndpointCriteria : EnvironmentEndpointBase, ICriteria
+    public class EnvironmentEndpoint : EnvironmentEndpointBase, IBaseObject
     {
         /// <summary>
-        /// Gets or sets the count.
+        /// Gets or sets the created by.
         /// </summary>
-        /// <value>The count.</value>
-        public int Count
+        /// <value>The created by.</value>
+        public string CreatedBy
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the created stamp.
+        /// </summary>
+        /// <value>The created stamp.</value>
+        public DateTime? CreatedStamp
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the last updated by.
+        /// </summary>
+        /// <value>The last updated by.</value>
+        public string LastUpdatedBy
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the last updated stamp.
+        /// </summary>
+        /// <value>The last updated stamp.</value>
+        public DateTime? LastUpdatedStamp
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Gets or sets the state.
+        /// </summary>
+        /// <value>The state.</value>
+        public ObjectState State
         {
             get; set;
         }
