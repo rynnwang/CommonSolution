ac241e9afe2a13874e7432854f717749afd7d421
Rynn Wang <rynn.wang@outlook.com>
2016-01-24 19:29:32 +0800

    no message

---
+++ Development/Beyova.Common.UnitTest/LDAPUnitTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Beyova.ProgrammingIntelligence;
+using Beyova.ApiTracking;
+using Beyova.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Beyova.Common.UnitTest
+{
+    [TestClass]
+    public class LDAPUnitTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var isAuthenticated = LdapExtension.TryAuthenticate("", "", "");
+            Assert.IsTrue(isAuthenticated);
+        }
+    }
+}
---
+++ Development/Beyova.Common/Extensions/LdapExtension.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Web;
+using Beyova.ExceptionSystem;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Beyova;
+using Beyova.RestApi;
+using System.Management;
+using System.DirectoryServices;
+
+namespace Beyova
+{
+    /// <summary>
+    /// Class LdapExtension.
+    /// </summary>
+    public static class LdapExtension
+    {
+        /// <summary>
+        /// Gets the directory entry.
+        /// </summary>
+        /// <param name="ldapPath">The LDAP path. Sample: LDAP://testhr.com/CN=Users,DC=testhr,DC=com</param>
+        /// <param name="userName">Name of the user. Sample: administrator</param>
+        /// <param name="password">The password. Sample: 123456</param>
+        /// <param name="authenticationType">Type of the authentication.</param>
+        /// <returns>DirectoryEntry.</returns>
+        public static DirectoryEntry GetDirectoryEntry(string ldapPath, string userName, string password, AuthenticationTypes authenticationType = AuthenticationTypes.Secure)
+        {
+            try
+            {
+                ldapPath.CheckEmptyString("ldapPath");
+                userName.CheckEmptyString("userName");
+                password.CheckEmptyString("password");
+
+                return new DirectoryEntry()
+                {
+                    Path = ldapPath,
+                    Username = userName,
+                    Password = password,
+                    AuthenticationType = authenticationType
+                };
+            }
+            catch (Exception ex)
+            {
+                throw ex.Handle("GetDirectoryEntry", new { ldapPath, userName, password });
+            }
+        }
+
+        /// <summary>
+        /// Tries the authenticate.
+        /// </summary>
+        /// <param name="ldapPath">The LDAP path.</param>
+        /// <param name="userName">Name of the user.</param>
+        /// <param name="password">The password.</param>
+        /// <returns><c>true</c> if authentication is passed, <c>false</c> otherwise.</returns>
+        public static bool TryAuthenticate(string ldapPath, string userName, string password)
+        {
+            try
+            {
+                var adEntry = GetDirectoryEntry(ldapPath, userName, password);
+
+                if (adEntry != null)
+                {
+                    adEntry.RefreshCache();
+                    return true;
+                }
+
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Checks the name of the user.
+        /// </summary>
+        /// <param name="entry">The entry.</param>
+        /// <param name="userName">Name of the user.</param>
+        /// <returns>
+        ///   <c>true</c> if user name is existed, <c>false</c> otherwise.</returns>
+        public static bool CheckUserName(this DirectoryEntry entry, string userName)
+        {
+            try
+            {
+                entry.CheckNullObject("entry");
+                userName.CheckEmptyString("userName");
+
+                var directorySearcher = new DirectorySearcher()
+                {
+                    SearchRoot = entry,
+                    Filter = "(&(objectClass=user) (cn=" + userName + "))"
+                };
+
+                if ((directorySearcher.FindAll()?.Count ?? 0) > 0)
+                {
+                    return true;
+                };
+
+                return false;
+            }
+            catch (Exception ex)
+            {
+                throw ex.Handle("CheckUserName", new { userName });
+            }
+        }
+    }
+}
